0f97892eae1ca5c1719c3e65a0a5454ea47c6bbd
William Mantly <wmantly@gmail.com>
2018-10-12 00:19:49 -0400

    teams

--- users/serializers.py
+++ users/serializers.py
@@ -2,10 +2,10 @@ from .models import Profile, Team
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Profile
-        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = Profile
+#         fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
 
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
