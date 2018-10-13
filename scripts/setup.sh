3e5d509e251cc26f09d8dcf7ce194a8f28cc471f
William Mantly <wmantly@gmail.com>
2018-10-13 15:54:04 -0400

    Added script for set up

---
+++ scripts/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+./scripts/subMudInit.sh
+
+if [ ! -d ./env ]; then
+	printf "\nVirtual environment not found, creating it\n"
+	sleep 1
+	virtualenv env -p python3
+fi
+
+printf "\nActivating virtual environment...\n"
+source env/bin/activate
+
+printf "\n\nInstalling python packages..\n\n"
+pip install -r requirements.txt
+
+if [ -d ./frontend ]; then
+	printf "\n\nInstalling Node packages...\n\n"
+	npm --prefix ./frontend/ install ./frontend/
+	
+	printf "\nAdding nodeJS modules bin to your path\n\n"
+	export PATH=`pwd`/frontend/node_modules/.bin/:$PATH
+fi
+
+printf "\n\nSetting up environment alias\n"
