5b635ee43c087cedfe756222c7d46a7361b76fa5
William Mantly <wmantly@gmail.com>
2018-10-14 01:09:54 -0400

    swagger

--- backend/project/settings/base.py
+++ backend/project/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'commons',
     'rest_registration',
     'users',
